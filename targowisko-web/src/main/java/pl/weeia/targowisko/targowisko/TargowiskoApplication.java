ba0ed61c79e7bb86e8c177d381ad55409afeb4d5
makz <maksymilian.regula@gmail.com>
2019-10-08 21:23:11 +0200

    init commit, added docker-compose for postgresql

---
+++ targowisko-web/src/main/java/pl/weeia/targowisko/targowisko/TargowiskoApplication.java
@@ -0,0 +1,13 @@
+package pl.weeia.targowisko.targowisko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargowiskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargowiskoApplication.class, args);
+	}
+
+}
